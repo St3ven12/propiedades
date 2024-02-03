ec423cdeeb2a91d486cf42ae681e2e5a68afe609
St3ven12 <stivenpamplona890@gmail.com>
2024-02-03 12:56:10 -0500

    cambio de titulo

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $result_tipos = obtenerTodosLosTipos();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SAWPI - Inmobiliaria</title>
+    <title>Propiedades</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="estilo.css">
 </head>
